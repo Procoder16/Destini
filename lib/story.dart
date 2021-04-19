c510a3944fd541d57406451f0e0c558ab085958e
Procoder16 <smsoumik1608@gmail.com>
2021-04-20 00:49:03 +0530

    updated the storyline and also the choice buttons

--- lib/story.dart
+++ lib/story.dart
@@ -11,9 +11,5 @@ class Story {
   String choice1;
   String choice2;
 
-  Story(storyTitle, choice1, choice2) {
-    storyTitle = this.storyTitle;
-    choice1 = this.choice1;
-    choice2 = this.choice2;
-  }
+  Story({this.storyTitle, this.choice1, this.choice2});
 }
